68fde3cbe4238fd6a29c83987a0d481a30110009
Sina Shahbeyk <sinashahbeyk@gmail.com>
2022-02-19 10:34:19 +0000

    django forms

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.shortcuts import redirect
 from django.utils import timezone
+from blog.forms import CommentForm
 from blog.models import Post
 
 # Create your views here.
@@ -8,6 +11,29 @@ def index(request):
     return render(request, "blog/index.html", {'posts':posts})
 
 
+# def post_detail(request, slug):
+#     post = get_object_or_404(Post, slug=slug)
+#     return render(request,"blog/post-detail.html",{"post":post})
+
+
 def post_detail(request, slug):
-    post = get_object_or_404(Post, slug=slug)
+    if request.user.is_active:
+        if request.method == "POST":
+            comment_form = CommentForm(request.POST)
+
+            if comment_form.is_valid():
+                comment = comment_form.save(commit=False)
+                comment.content_object = post
+                comment.creator = request.user
+                comment.save()
+                return redirect(request.path_info)
+        else:
+            comment_form = CommentForm()
+    else:
+        comment_form = None
+    return render(
+        request, "blog/post-detail.html", {"post": post, "comment_form": comment_form}
+    )
     return render(request,"blog/post-detail.html",{"post":post})
+
+    
